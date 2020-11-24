2db6b77574d7bf49c5b5e13f9216dc13175c05cd
adrianababilonia22 <72059895+adrianababilonia22@users.noreply.github.com>
2020-11-24 16:11:34 -0500

    Create ejercicio2.py

---
+++ ejercicio2.py
@@ -0,0 +1,38 @@
+'''
+-----------------------------
+ EJERCICIO N°2
+ Instrucciones raise y assert
+-----------------------------
+'''
+# LA INSTRUCCIÓN RAISE
+# 1era aplicación
+def mate(n):
+  raise ZeroDivisionError
+
+try:
+  mate(0)
+except ArithmeticError:
+  print("¿Que pasó? ¿Un error?")
+
+# 2da aplicación
+def mate(n):
+  try:
+    return n / 0
+  except:
+    print("¡Lo hice otra vez!")
+    raise
+
+try:
+  mate(0)
+except ArithmeticError:
+  print("¡Ya veo!")
+
+# ----------------------------
+# ASSERT
+import math
+
+x = float(input("Ingresa un numero: "))
+assert x >= 0.0
+x = math.sqrt(x)
+
+print(x)
